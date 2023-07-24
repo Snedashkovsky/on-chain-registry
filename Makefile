aa68161639e2a1850133dc1157a2864b89a96293
Snedashkovsky <sn@cyberdrop.ai>
2023-07-24 14:45:40 +0800

    - add code building, code storing and contract instantiation - add multichain contract export - refactor makefile and main python script - update requirements

--- Makefile
+++ Makefile
@@ -2,7 +2,21 @@ extract:
 	@echo "pull chain-registry"
 	git submodule init
 	git submodule update --remote
-	python3 asset_data.py extract
+	@echo "extract data"
+	python3 asset_data.py --extract=True --export=False
 export:
-	python3 asset_data.py export
+	@echo "export data"
+	python3 asset_data.py --extract=False --export=True
 update: extract export
+build_code:
+	@echo "build code"
+	python3 contract_deploy.py --build_code=True
+store_code:
+	@echo "store code"
+	python3 contract_deploy.py --chain_name=$(chain_name) --store_code=True
+init_contract:
+	@echo "instantiate contract"
+	python3 contract_deploy.py --chain_name=$(chain_name) --init_contract=True
+deploy_contract:
+	@echo "build a code, store a code and instantiate a contract"
+	python3 contract_deploy.py --chain_name=$(chain_name) --build_code=True --store_code=True --init_contract=True
--- config.py
+++ config.py
@@ -5,7 +5,6 @@ from dotenv import dotenv_values
 from cyber_sdk.client.lcd import LCDClient
 from cyber_sdk.key.mnemonic import MnemonicKey
 
-
 # logging config
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(message)s',
@@ -20,12 +19,49 @@ PUSSY_NODE_LCD_URL = 'https://lcd.space-pussy.cybernode.ai'
 INIT_CHAIN_ID_NAME_DICT = {PUSSY_CHAIN_ID: PUSSY_CHAIN_NAME}
 INIT_CHAIN_ID_LCD_DICT = {PUSSY_CHAIN_ID: [PUSSY_NODE_LCD_URL]}
 
-# for export to a contract
-CHAIN_ID = 'bostrom'
-NODE_RPC_URL = 'https://rpc.bostrom.cybernode.ai:443'
-NODE_LCD_URL = 'https://lcd.bostrom.cybernode.ai'
-CONTRACT_ADDRESS = 'bostrom1eeahgvdsun8a04rh5vy9je49nllq6nj8ljmaslsvjeyg0j0063mssjcjmt'
-LCD_CLIENT = LCDClient(url=NODE_LCD_URL, chain_id=CHAIN_ID)
+# for contract instantiations
+CONTRACT_NAMES = {
+    'bostrom': 'test-on-chain-registry',
+    'localbostrom': 'test-on-chain-registry'
+}
+CODE_IDS = {
+    'bostrom': '28',
+    'localbostrom': '46'
+}
+
+# for export to contracts
+EXPORT_CHAINS = ['bostrom']
+CHAIN_IDS = {
+    'bostrom': 'bostrom',
+    'localbostrom': 'localbostrom'
+}
+PREFIXES = {
+    'bostrom': 'bostrom',
+    'localbostrom': 'bostrom'
+}
+FEE_DENOMS = {
+    'bostrom': 'boot',
+    'localbostrom': 'boot'
+}
+NODE_RPC_URLS = {
+    'bostrom': 'https://rpc.bostrom.cybernode.ai:443',
+    'localbostrom': 'http://localhost:26657'
+}
+NODE_LCD_URLS = {
+    'bostrom': 'https://lcd.bostrom.cybernode.ai',
+    'localbostrom': 'http://localhost:1317'
+}
+CONTRACT_ADDRESSES = {
+    'bostrom': 'bostrom1eeahgvdsun8a04rh5vy9je49nllq6nj8ljmaslsvjeyg0j0063mssjcjmt',
+    'localbostrom': 'bostrom1jskphr9zxs2yp3e9vwamyeqwxkfkg5zghyv52qcy4tlql3qjjwgsk2d9vp'
+}
+
+LCD_CLIENTS = {_network: LCDClient(url=NODE_LCD_URLS[_network],
+                                   chain_id=CHAIN_IDS[_network],
+                                   prefix=PREFIXES[_network])
+               for _network in CHAIN_IDS.keys()}
 WALLET_SEED = dotenv_values('.env')['WALLET_SEED']
-WALLET = LCD_CLIENT.wallet(MnemonicKey(mnemonic=WALLET_SEED))
-WALLET_ADDRESS = WALLET.key.acc_address
+WALLETS = {_network: LCD_CLIENTS[_network].wallet(MnemonicKey(mnemonic=WALLET_SEED))
+           for _network in CHAIN_IDS.keys()}
+WALLET_ADDRESSES = {_network: WALLETS[_network].key.acc_address
+                    for _network in CHAIN_IDS.keys()}
